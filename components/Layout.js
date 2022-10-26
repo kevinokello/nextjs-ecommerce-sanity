ffe1b8d71a76dfc80d6b8b9deb9ae0f6d6a4fcb8
kevinokello <okellokevin77@gmail.com>
2022-10-26 12:53:40 +0300

    react context

--- components/Layout.js
+++ components/Layout.js
@@ -1,20 +1,39 @@
-import React from "react";
 import { createTheme } from "@mui/material/styles";
 import {
   AppBar,
+  Badge,
   Box,
+  Button,
   Container,
   CssBaseline,
+  Divider,
+  Drawer,
+  IconButton,
+  InputBase,
   Link,
+  List,
+  ListItem,
+  ListItemText,
+  Menu,
+  MenuItem,
+  Switch,
   ThemeProvider,
   Toolbar,
   Typography,
+  useMediaQuery,
 } from "@mui/material";
 import Head from "next/head";
 import NextLink from "next/link";
 import classes from "../utils/classes";
+import { useContext, useEffect, useState } from "react";
+import { Store } from "../utils/Store";
+import jsCookie from "js-cookie";
+import { useRouter } from "next/router";
 
 export default function Layout({ title, description, children }) {
+  const router = useRouter();
+  const { state, dispatch } = useContext(Store);
+  const { darkMode } = state;
   const theme = createTheme({
     components: {
       MuiLink: {
@@ -36,6 +55,7 @@ export default function Layout({ title, description, children }) {
       },
     },
     palette: {
+      mode: darkMode ? "dark" : "light",
       primary: {
         main: "#f0c000",
       },
@@ -44,6 +64,11 @@ export default function Layout({ title, description, children }) {
       },
     },
   });
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? "DARK_MODE_OFF" : "DARK_MODE_ON" });
+    const newDarkMode = !darkMode;
+    jsCookie.set("darkMode", newDarkMode ? "ON" : "OFF");
+  };
 
   return (
     <>
@@ -55,18 +80,26 @@ export default function Layout({ title, description, children }) {
         <CssBaseline />
         <AppBar position="static" sx={classes.appbar}>
           <Toolbar sx={classes.toolbar}>
-            <NextLink href="/" passHref>
-              <Link>
-                <Typography sx={classes.brand}>amazona</Typography>
-              </Link>
-            </NextLink>
+            <Box display="flex" alignItems="center">
+              <NextLink href="/" passHref>
+                <Link>
+                  <Typography sx={classes.brand}>amazona</Typography>
+                </Link>
+              </NextLink>
+            </Box>
+            <Box>
+              <Switch
+                checked={darkMode}
+                onChange={darkModeChangeHandler}
+              ></Switch>
+            </Box>
           </Toolbar>
         </AppBar>
         <Container component="main" sx={classes.main}>
           {children}
         </Container>
         <Box component="footer" sx={classes.footer}>
-          <Typography>All rights reserved. KevCodes.</Typography>
+          <Typography>All rights reserved. Sanity Amazona.</Typography>
         </Box>
       </ThemeProvider>
     </>
